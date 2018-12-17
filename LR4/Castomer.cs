d0539c564d92b6ef12456a9c081817c2d50ebc30
HarryP7 <gish177@rambler.ru>
2018-12-18 00:20:57 +0300

    second commit!

--- LR4/Castomer.cs
+++ LR4/Castomer.cs
@@ -11,28 +11,22 @@ namespace LR4
         public static string Pass;
         public static String chekLogin(String Login)
         {
-            List<string> List = new List<string> { "Kontinenent@gmail.com", "Lucoil@gmail.com", "Megafon@gmail.com", "Tehnostroy@gmail.com" };
-            if (Login.Length < 4 &&
-                Regex.IsMatch(Login, @"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$"))
-                return "Введите корректный E-mail, более 4х символов.";
-            return "";
+            
         }
     }
     class Castomer
     {
         public static String plaseWork;
-
-        public static String chekPlaseWork(String plaseWork)
-        {
-            List<string> List = new List<string> {"OOO Континент", "ПАО Лукойл", "ПАО МегаФон", "ООО Трансстрой" };
+        public static String chekPlaseWork(String plaseWork){
+            List<string> List = new List<string> {"OOO Континент", "ПАО Лукойл", "ПАО МегаФон", "ООО " };
             for (int j = 0; j < List.Count; j++)
             {
                 if (plaseWork == List[j])
                 {
-                    return "Фирма корректна";
+                    return "";
                 }
             }
-            return "Фирма не существует\nПовтотрите ввод";
+            return "";
         }
     }
 }
